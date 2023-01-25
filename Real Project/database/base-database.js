5d6bedb8accb95e08f387fe4c1eaaeb7e2dc1d27
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-01-25 23:55:04 +0300

    async await apis

--- Real Project/database/base-database.js
+++ Real Project/database/base-database.js
@@ -7,26 +7,39 @@ class BaseDatabase{
         this.filename = model.name
     }
     //With callback
-    save(objects, callback = () => {console.log("internal write driver")}){
-        fs.writeFile(`${__dirname}/${this.filename}.json`, flatted.stringify(objects, null, 2),callback)
-    }
-    
-    load(){
-        const file = fs.readFileSync(`${__dirname}/${this.filename}.json`, 'utf8')
-        const objects = flatted.parse(file)
-        return objects.map(this.model.create)
+    save(objects){
+        return new Promise((resolve,reject) => {
+            fs.writeFile(`${__dirname}/${this.filename}.json`, flatted.stringify(objects, null, 2), (err) => {
+                if (err) reject(err)
+                resolve()
+            })
+        })
     }
     //With callback
-    insert (object, callback = () => {}){
-        const objects = this.load()
+    load(){
+        return new Promise((resolve,reject) => {
+            fs.readFile(`${__dirname}/${this.filename}.json`, 'utf8',(err,file) =>{
+                if(err) return reject(err)
+                const objects = flatted.parse(file)
+    
+                resolve(objects.map(this.model.create)) //callback(err,file)
+            })
+        })
+    }
+    //With callback
+    //In this api first time we using load and save api
+    async insert (object){
+        const objects = await this.load()
         objects.push(object)
-        this.save(objects, callback)
+        return this.save(objects)
+
     }
     
     remove(index){
-        const objects = this.load()
-        objects.splice(index, 1)
-        this.save(objects)
+        this.load((err,objects) => {
+            objects.splice(index, 1)
+            this.save(objects)
+        })
     }
 
     update(object){
@@ -35,8 +48,17 @@ class BaseDatabase{
         objects.splice(index, 1, object)
         this.save(objects)
     }
+    //With promise
+
     findBy(property, value){
         return this.load().find(o => o[property] == value)
+    }
+    //With promise
+    find(id, callback){
+        this.load((err,objects) => {
+            if (err) return callback(err)
+            callback(null, objects.find(o => o.id == id))
+        })
     }
 }
 
