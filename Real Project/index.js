7066cb6d6124362266f27da5a4b531a602b9d054
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-01 00:11:01 +0300

    body-parser post request and new user

--- Real Project/index.js
+++ Real Project/index.js
@@ -1,8 +1,11 @@
 const {passengerDatabase} = require('./database')
+const Passenger = require('./models/passenger')
+const bodyParser = require('body-parser')
 const flatted = require('flatted')
 const express = require('express') 
 
 const app = express()
+app.use(bodyParser.json())
 const port = 3000
 
 app.set('view engine','pug')
@@ -16,11 +19,19 @@ app.get('/passengers', async (req,res) =>{
     // res.send(flatted.stringify(passengers))
     res.render('passengers', {passengers})
 })
+//Post request for new Passenger
+app.post('/passengers', async(req, res, next) => {
+    const newPerson = Passenger.create(req.body)
+    await passengerDatabase.insert(newPerson)
+    //res.writeHead(200, {'content-type': 'text/html'})
+    res.send(newPerson)
+})
 
-app.get('/passenger/__id__', async (req,res) =>{
-    const passengers = await passengerDatabase.load()
-    // res.send(flatted.stringify(passengers))
-    res.render('passengers', {passengers})
+//Detail
+app.get('/passengers/:passengerId', async (req,res) =>{
+    const passenger = await passengerDatabase.find(req.params.passengerId)
+    if(!passenger) return res.status(404).send('Cannot find passenger')
+    res.render('passenger', {passenger})
 })
 
 app.listen(port, () =>{
