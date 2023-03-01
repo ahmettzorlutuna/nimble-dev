18b68b929a9bdd26f41de6f0b00f84b3feebe22c
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-01 23:48:31 +0300

    Driver id added

--- Real Project/models/driver.js
+++ Real Project/models/driver.js
@@ -1,10 +1,14 @@
+const uuid = require('uuid')
+
 class Driver {
-    constructor(name, location) {
+    constructor(name, location, id = uuid.v4()) {
+        this.id = id
         this.name = name;
         this.location = location;
+
     }
-    static create({name, location}){
-        return new Driver(name,location)
+    static create({name, location, id}){
+        return new Driver(name,location, id)
     }
 }
 
