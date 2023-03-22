84813c87c5e0894ce9f6a58a5658b49ffadb899f
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-23 00:18:17 +0300

    index

--- Real Project/index.js
+++ Real Project/index.js
@@ -2,10 +2,11 @@ const PassengersRouter = require('./routes/passengers')
 const IndexRouter = require('./routes/index')
 const bodyParser = require('body-parser')
 const express = require('express') 
+require('./mongo-connection')
 
 const app = express()
 app.use(bodyParser.json())
-const port = 3000
+const port = 3001
 
 app.set('view engine','pug')
 app.use('/passengers', PassengersRouter)
