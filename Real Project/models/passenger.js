4ae91e022b7c0a4ebc1197bb59282a2ff599b8a9
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-23 23:59:44 +0300

    Created Passenger Schema and model

--- Real Project/models/passenger.js
+++ Real Project/models/passenger.js
@@ -1,21 +1,32 @@
-const Booking = require('./booking')
-const uuid = require('uuid')
-class Passenger {
-    constructor(name, location, bookings = [], id = uuid.v4()) {
-        this.id = id
-        this.name = name
-        this.location = location
-        this.bookings = bookings
-    }
-    book(driver, origin = undefined, destination = undefined) {
-        const booking = new Booking(driver, this, origin, destination)
-        this.bookings.push(booking)
-        return booking
-    }
+// const Booking = require('./booking')
+// const uuid = require('uuid')
 
-    static create({name, location, bookings, id}){
-        return new Passenger(name,location,bookings, id)
-    }
-}
+const mongoose = require('mongoose')
 
-module.exports = Passenger
+const PassengerSchema = new mongoose.Schema({
+    name: String,
+    location: String,
+    bookings: []
+})
+
+module.exports = mongoose.model('Passenger', PassengerSchema)
+
+// class Passenger {
+//     constructor(name, location, bookings = [], id = uuid.v4()) {
+//         this.id = id
+//         this.name = name
+//         this.location = location
+//         this.bookings = bookings
+//     }
+//     book(driver, origin = undefined, destination = undefined) {
+//         const booking = new Booking(driver, this, origin, destination)
+//         this.bookings.push(booking)
+//         return booking
+//     }
+
+//     static create({name, location, bookings, id}){
+//         return new Passenger(name,location,bookings, id)
+//     }
+// }
+
+// module.exports = Passenger
