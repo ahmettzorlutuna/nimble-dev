8e8075d9c9cb7ccaf8c391236ae9bbfc91fc637b
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-23 23:57:41 +0300

    Mongo db connect path change

--- Real Project/mongo-connection.js
+++ Real Project/mongo-connection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/RealProject");
+  await mongoose.connect("mongodb://127.0.0.1:27017/Nimble");
   console.log("Connected the DB !!");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
