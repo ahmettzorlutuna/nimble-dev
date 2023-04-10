acbf9b2b642a4c4c422174b5b25b9c596a0e34f9
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-10 23:37:58 +0300

    Driver Passenger ref autopopulate true

--- Real Project/models/booking.js
+++ Real Project/models/booking.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 
 const BookingSchema = new mongoose.Schema({
-    driver: {},
-    passenger : {},
+    driver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Driver',
+        autopopulate: true
+    },
+    passenger : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Passenger',
+        autopopulate: true
+    },
     origin: String,
     destination: String
 })
+
+BookingSchema.plugin(require('mongoose-autopopulate'));
 
 module.exports = mongoose.model('Booking', BookingSchema)
