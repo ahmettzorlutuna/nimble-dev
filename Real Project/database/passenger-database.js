adec18479e22f44e37b9e883069f43ad171407da
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-12 23:17:49 +0300

    passengerdb book method

--- Real Project/database/passenger-database.js
+++ Real Project/database/passenger-database.js
@@ -1,13 +1,23 @@
 const BaseDatabase = require('./base-database')
 const Passenger = require('../models/passenger')
+const bookingDatabase = require('./booking-database')
+
 
 class PassengerDatabase extends BaseDatabase{
-    findByName(name){
+    async findByName(name){
         return this.findBy('name', name)
     }
 
-    findByLocation(location){
+    async findByLocation(location){
         return this.findBy('location', location)
+    }
+
+    async book(driver, passenger, origin, destination){
+        const booking = await bookingDatabase.insert({driver, passenger, origin, destination})
+        // this.bookings.push(booking) //Circular dependencies 
+        passenger.bookings.push(booking)
+        await passenger.save()
+        return booking
     }
 }
 
