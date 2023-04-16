b943f3b6bcb61262a9c191179e0c49c83e3f39c0
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-16 04:42:52 +0300

    variable name change

--- Real Project/routes/drivers.js
+++ Real Project/routes/drivers.js
@@ -1,29 +1,29 @@
-const driverDatabase = require('../database/driver-database')
+const driverService = require('../services/driver-service')
 const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req,res) =>{
-    const drivers = await driverDatabase.load()
+    const drivers = await driverService.load()
     // res.send(flatted.stringify(drivers))
     res.render('drivers', {drivers})
 })
 //Post request for new driver
 router.post('/', async(req, res, next) => {
-    const newPerson = await driverDatabase.insert(req.body)
+    const newPerson = await driverService.insert(req.body)
     //res.writeHead(200, {'content-type': 'text/html'})
     res.send(newPerson)
 })
 
 //Detail
 router.get('/:driverId', async (req,res) =>{
-    const driver = await driverDatabase.find(req.params.driverId)
+    const driver = await driverService.find(req.params.driverId)
     if(!driver) return res.status(404).send('Cannot find driver')
     res.render('driver', {driver})
 })
 
 //driver Delete
 router.delete('/:driverId', async(req,res) =>{
-    await driverDatabase.removeBy('_id',req.params.driverId)
+    await driverService.removeBy('_id',req.params.driverId)
     res.end('OK')
 })
 
@@ -31,7 +31,7 @@ router.delete('/:driverId', async(req,res) =>{
 router.patch('/:driverId', async(req,res) => {
     const {driverId} = req.params
     const {name} = req.body
-    await driverDatabase.update(driverId, {name}) // passed arg {name} is {name: 'User1'}  name is User1
+    await driverService.update(driverId, {name}) // passed arg {name} is {name: 'User1'}  name is User1
     res.send('ok')
 })
 
