94f59ba512026964cfbf4d4e7557046f712f3336
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-16 04:41:01 +0300

    Driver Service

---
+++ Real Project/services/driver-service.js
@@ -0,0 +1,14 @@
+const BaseDatabase = require('./base-service')
+const Driver = require('../models/driver')
+
+class DriverDatabase extends BaseDatabase{
+    async findByDriverName(name){
+        return this.findBy('name', name)
+    }
+
+    async findByLocation(location){
+        return this.findBy('location', location)
+    }
+}
+
+module.exports = new DriverDatabase(Driver) //new keyword for singleton pattern
