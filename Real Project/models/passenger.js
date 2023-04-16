7073071a9a7355eeea3f6958427c258e457b19cc
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-16 04:43:10 +0300

    Populate max depth 2

--- Real Project/models/passenger.js
+++ Real Project/models/passenger.js
@@ -9,20 +9,12 @@ const PassengerSchema = new mongoose.Schema({
     bookings: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Booking', 
-        autopopulate: true
+        autopopulate: { maxDepth: 2}
     }]
 })
 
 PassengerSchema.plugin(require('mongoose-autopopulate'));
 
-PassengerSchema.methods.book = async function(driver, origin, destination){
-    // const booking = new Booking(driver, this, origin, destination) //Old method. İf we do it like this we need to update again.
-    const booking = await Booking.create({driver, passenger: this, origin, destination})
-    // this.bookings.push(booking) //Circular dependencies 
-    this.bookings.push(booking) 
-    await this.save()
-    return booking
-}
 
 module.exports = mongoose.model('Passenger', PassengerSchema)
 
