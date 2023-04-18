9d70da6ed478f2e184c327a5403429b38d50f16e
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-18 23:20:09 +0300

    Booking service and book method added to there from passengerservice

--- Real Project/services/booking-service.js
+++ Real Project/services/booking-service.js
@@ -1,5 +1,7 @@
 const BaseDatabase = require('./base-service')
 const Booking = require('../models/booking')
+const driverService = require('./driver-service')
+const passengerService = require('./passenger-service')
 
 class BookingDatabase extends BaseDatabase{
     async findByPassengerId(passengerId){
@@ -8,6 +10,17 @@ class BookingDatabase extends BaseDatabase{
 
     async findByDriverId(driverId){
         return this.findBy('driver', driverId)
+    }
+    //Passenger booking service
+    async book(driverId, passengerId, origin, destination){
+        const passenger = await passengerService.find(passengerId)
+        const driver = await driverService.find(driverId) 
+
+        const booking = await this.insert({driver, passenger, origin, destination})
+        // this.bookings.push(booking) //Circular dependencies 
+        passenger.bookings.push(booking)
+        await passenger.save()
+        return booking
     }
 }
 
