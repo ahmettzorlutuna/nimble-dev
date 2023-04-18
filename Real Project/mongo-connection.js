fc3dbf1ccaf552cbc60f5c985d4eb516af4ff43c
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-18 23:22:20 +0300

    Mongo connection on local

--- Real Project/mongo-connection.js
+++ Real Project/mongo-connection.js
@@ -6,5 +6,5 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/Nimblee");
   console.log("Connected the DB !!");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your services has auth enabled
 }
