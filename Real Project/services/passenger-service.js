848af7e1fe494176b3155557f98a65596e6b2d00
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-18 23:21:08 +0300

    Passenger Services

--- Real Project/services/passenger-service.js
+++ Real Project/services/passenger-service.js
@@ -1,8 +1,5 @@
 const BaseDatabase = require('./base-service')
 const Passenger = require('../models/passenger')
-const bookingDatabase = require('./booking-service')
-const driverDatabase = require('./driver-service')
-
 
 class PassengerDatabase extends BaseDatabase{
     async findByName(name){
@@ -11,17 +8,6 @@ class PassengerDatabase extends BaseDatabase{
 
     async findByLocation(location){
         return this.findBy('location', location)
-    }
-    //Passenger booking service
-    async book(driverId, passengerId, origin, destination){
-        const passenger = await this.find(passengerId)
-        const driver = await driverDatabase.find(driverId) 
-
-        const booking = await bookingDatabase.insert({driver, passenger, origin, destination})
-        // this.bookings.push(booking) //Circular dependencies 
-        passenger.bookings.push(booking)
-        await passenger.save()
-        return booking
     }
 }
 
