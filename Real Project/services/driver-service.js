45c5f99335d36b89fce65277d37b8c1c37fd0ea2
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-18 23:20:43 +0300

    Find young drivers routes

--- Real Project/services/driver-service.js
+++ Real Project/services/driver-service.js
@@ -9,6 +9,14 @@ class DriverDatabase extends BaseDatabase{
     async findByLocation(location){
         return this.findBy('location', location)
     }
+    async findYoungDrivers(){
+        // return this.model.find({age:{$lt:30}})
+        return this.query({
+            age:{
+                $lt: 30
+            }
+        })
+    }
 }
 
 module.exports = new DriverDatabase(Driver) //new keyword for singleton pattern
