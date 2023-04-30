8522163a603c3f3810b1fffd4191cad900c73ae4
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-05-01 00:47:07 +0300

    print booking his. test

--- Real Project/lib/__tests__/print-booking-history.test.js
+++ Real Project/lib/__tests__/print-booking-history.test.js
@@ -1,6 +1,27 @@
-const printAllBookings = require('../print-booking-history')
+const printAllBookings = require("../print-booking-history");
 
-test('print passenger bookings', () => {
-    const rv = printAllBookings(passenger)
-    expect(rv).toBe(3);
-})
+test("print passenger bookings", () => {
+    //İnit
+  const passenger = {
+    name: "Armagan",
+    bookings: [
+      {
+        passenger: { name: "Armagan" },
+        driver: { name: "Stefan" },
+        origin: "Kreuzberg",
+        destination: "Neukolln",
+      },
+    ],
+  };
+
+  const consoleSpy = jest.spyOn(console, 'log')
+
+  //Test
+  printAllBookings(passenger);
+
+  //Assertions
+  expect(consoleSpy).toHaveBeenCalledWith('Armagan is going Neukolln from Kreuzberg now. Estimated time not calculated yet.')
+  
+  //TearDown
+  consoleSpy.mockRestore()
+});
