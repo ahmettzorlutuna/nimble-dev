c5c6ad9984b571e127e8ee9631b6d074c1962457
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-05-03 23:44:38 +0300

    server.close()

--- Real Project/tests/passengers.test.js
+++ Real Project/tests/passengers.test.js
@@ -1,19 +1,15 @@
-const app = require('../index');
-const mongoose = require("mongoose");
-
-const request = require('supertest')(app);
+const app = require('../')
+const server = require('../')
+const request = require('supertest')(app)
 
 test('create a new passenger', async () => {
-    //Init
     const passengerToCreate = {
-        name: 'Test passenger',
-        location: 'Nef'
+        name: 'Ahmet',
+        location: 'Canada'
     }
+    const response = await request.post('/passengers').send(passengerToCreate)
 
-    const response = await request
-        .post('/passengers')
-        .send(passengerToCreate)
-        .expect(200)
-    
-        expect(response.body).toMatchObject(passengerToCreate)
+    expect(response.status).toBe(200)
 })
+
+afterAll(() => server.close())
