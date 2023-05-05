c6d70b6b32c05187059b4825e2d36e301e315585
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-05-06 00:18:53 +0300

    coonection string

--- Real Project/mongo-connection.js
+++ Real Project/mongo-connection.js
@@ -5,6 +5,6 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/Nimblee");
-  console.log("Connected the DB !!");
+  console.log("Connected the DB !!")
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your services has auth enabled
 }
